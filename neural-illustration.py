7435b0d01675c1201c87c1639d9175158266da6a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-08-23 23:09:46 -0400

    Add settings and global variables

--- neural-illustration.py
+++ neural-illustration.py
@@ -3,3 +3,14 @@ import tensorflow as tf
 from tensorflow.keras import layers, optimizers
 import matplotlib.pyplot as plt
 import time
+
+R = 20
+timesteps = 1
+
+resolution = (R, R)
+sample = np.zeros(resolution)
+sample[0:R//3, 0:R//3] = 1
+# neural illustration via dynamic programming
+
+actions = np.zeros((1, (timesteps * 3 + 1)))
+rewards = np.zeros((1, 1))
